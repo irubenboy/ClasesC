702042015337e1fea0f93636448c633107b90c25
Rubeb <rubenmoreno.saladillo@gmail.com>
2020-11-10 16:18:46 +0100

    Se ha añadido carpeta con ejemplos

---
+++ Ejemplos Rubén/sin_titulo.cpp
@@ -0,0 +1,10 @@
+#include <iostream> /* Importar librerias */
+using namespace std; /* Usar espacio de nombre*/
+
+/* Función main donde comienza la ejecución de nuestro programa */
+int main(int argc, char *argv[]) {
+	
+	cout << "Hola mundo!!"; //  Imprime por pantala "Hola Mundo"
+	return 0;
+}
+
